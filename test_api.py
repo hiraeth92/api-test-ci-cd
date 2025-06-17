fe2a123550ab3dc49238f666fbad030046e80fac
chaing <bossun113@gmail.com>
2025-06-17 17:46:39 +0800

    完成 API 測試與 CI/CD 設定

--- test_api.py
+++ test_api.py
@@ -1,7 +1,8 @@
 import requests
 
-def test_get_users():
-    url = "https://reqres.in/api/users?page=2"
+def test_httpbin_get():
+    url = "https://httpbin.org/get"
     response = requests.get(url)
+    print(f"狀態碼: {response.status_code}")
+    print(f"回應內容: {response.text}")
     assert response.status_code == 200
-    assert "data" in response.json()
