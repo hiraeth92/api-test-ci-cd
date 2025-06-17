7bc363dc1908c50d4af718d15d4106131f09aa0e
chaing <bossun113@gmail.com>
2025-06-17 15:38:32 +0800

    Initial commit with test script

---
+++ test_api.py
@@ -0,0 +1,7 @@
+import requests
+
+def test_get_users():
+    url = "https://reqres.in/api/users?page=2"
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert "data" in response.json()
