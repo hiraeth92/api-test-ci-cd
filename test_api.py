182d0c14c0f3d2b4eb32a6da1f3cfa3c901b5587
chaing <bossun113@gmail.com>
2025-06-17 18:33:47 +0800

    Add test cases and workflow config

--- test_api.py
+++ test_api.py
@@ -1,8 +1,27 @@
 import requests
 
-def test_httpbin_get():
-    url = "https://httpbin.org/get"
-    response = requests.get(url)
-    print(f"狀態碼: {response.status_code}")
-    print(f"回應內容: {response.text}")
+def test_get_users():
+    url = "https://reqres.in/api/users?page=2"
+    headers = {"x-api-key": "reqres-free-v1"}
+    response = requests.get(url, headers=headers)
     assert response.status_code == 200
+
+def test_create_user():
+    url = "https://reqres.in/api/users"
+    headers = {"x-api-key": "reqres-free-v1"}
+    payload = {"name": "morpheus", "job": "leader"}
+    response = requests.post(url, json=payload, headers=headers)
+    assert response.status_code == 201
+
+def test_update_user():
+    url = "https://reqres.in/api/users/2"
+    headers = {"x-api-key": "reqres-free-v1"}
+    payload = {"name": "morpheus", "job": "zion resident"}
+    response = requests.put(url, json=payload, headers=headers)
+    assert response.status_code == 200
+
+def test_delete_user():
+    url = "https://reqres.in/api/users/2"
+    headers = {"x-api-key": "reqres-free-v1"}
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 204
